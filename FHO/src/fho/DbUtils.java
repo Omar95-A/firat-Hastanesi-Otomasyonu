46f4df9a5d5ea77d4ae8377704463c09bb7b897e
Omar95-A <81801245+Omar95-A@users.noreply.github.com>
2022-04-07 02:48:57 -0700

    FHO Project Folder

---
+++ FHO/src/fho/DbUtils.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fho;
+
+import java.sql.ResultSet;
+import javax.swing.table.TableModel;
+/**
+ *
+ * @author OmarA
+ */
+public class DbUtils {
+        static TableModel resultSetToTableModel(ResultSet Rs) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
